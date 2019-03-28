df313f17e7860f645aced2453aa36454ad60734c
pixcai <pixcai@163.com>
2019-03-28 11:03:25 +0800

    完成注册

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { user } from '@/utils/api';
+import roles from '@/utils/roles';
 
-export default () => {
+export default ({ history }) => {
+  const [values, setValues] = useState({});
+  const onSubmit = () => {
+    const { name, key } = values;
+
+    if (name && name.length && key && key.length) {
+      user.create({
+        data: { name, key, role: roles.user.STUDENT },
+      }).then(() => history.push('/login'));
+    }
+  };
+  const setField = name => e => setValues({ ...values, [name]: e.target.value.trim() });
+
   return (
     <div className="bglog" style={{ height: '100vh' }}>
       <div className="container p-t-60">
@@ -11,33 +25,20 @@ export default () => {
             <div className=" card-box m-t-40">
               <h3 className="text-center uppercase">注册</h3>
               <div className="panel-body">
-                <form className="form-horizontal m-t-30" >
+                <form className="form-horizontal m-t-30">
                   <div className="form-group ">
                     <div className="col-12">
-                      <input className="form-control" type="text" required="" placeholder="手机号" />
-                    </div>
-                  </div>
-                  <div className="form-group row" style={{ marginRight: 0, marginLeft: 0 }}>
-                    <div className="col-12 col-md-7 ">
-                      <input className="form-control" type="password" required placeholder="请输入验证码" />
-                    </div>
-                    <div className="col-12 col-md-5 m-t-20">
-                      <button className="btn loginbnt  btn-block  savecouse" type="submit">获取验证码</button>
+                      <input className="form-control" onChange={setField('name')} type="text" required placeholder="用户名" />
                     </div>
                   </div>
                   <div className="form-group">
                     <div className="col-12">
-                      <input className="form-control" type="password"required="" placeholder="请输入验密码" />
-                    </div>
-                  </div>
-                  <div className="form-group">
-                    <div className="col-12">
-                      <input className="form-control" type="password" required="" placeholder="邀请码（可选）" />
+                      <input className="form-control" onChange={setField('key')} type="password" required placeholder="密码" />
                     </div>
                   </div>
                   <div className="form-group text-center m-t-40">
                     <div className="col-12">
-                      <button className="btn loginbnt  btn-block" type="submit">注册</button>
+                      <button className="btn loginbnt  btn-block" onClick={onSubmit}>注册</button>
                     </div>
                   </div>
                   <div className="form-group m-t-30 m-b-0">
