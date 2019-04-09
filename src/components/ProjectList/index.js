c434fbdcdf8edcd8056b564758bdfc68752ed850
pixcai <pixcai@163.com>
2019-04-09 12:25:54 +0800

    Add timeConsume to project title

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -10,6 +10,7 @@ export default ({ data = [] }) => {
             <h5 className="mb-0">
               <button className="btn " data-toggle="collapse" data-target="#collapsecourse1" aria-expanded="true" aria-controls="collapsecourse1">
                 {project.title}
+                <span style={{ fontSize: 13, marginLeft: 10 }}>(耗时：{project.timeConsume})</span>
               </button>
               <a href="#" className="palyico" data-toggle="modal" data-target="#studentlist">▶</a>
               <a href="#" className="stopico">▪</a>
@@ -18,7 +19,7 @@ export default ({ data = [] }) => {
           <div id="collapsecourse1" className="collapse " aria-labelledby="headingcourse1" data-parent="#accordioncourse">
             {get(project, 'labs', []).map(lab => (
               <div key={lab.id} className="list-group">
-                <a className="list-group-item list-group-item-action " data-toggle="modal" data-target="#studentlist"><span className="learnLesson all"><i /></span>→ {lab.name}<span className="time">耗时10分钟</span></a>
+                <a className="list-group-item list-group-item-action " data-toggle="modal" data-target="#studentlist"><span className="learnLesson all"><i /></span>→ {lab.name}</a>
               </div>
             ))}
           </div>
