9d8c50082e807a3e4be6e252fc3b6d7a6da2cb54
pixcai <pixcai@163.com>
2019-04-12 17:16:04 +0800

    Start and Stop Project

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import get from 'lodash-es/get';
+import { Dialog } from '@alifd/next';
 
-export default ({ data = [] }) => {
+export default ({ course, data = [] }) => {
+  const [isRunning, setIsRunning] = useState({});
+  const onClick = () => Dialog.alert({
+    title: '提示',
+    content: '请先运行项目',
+  });
+  const onStart = (id) => {
+    Dialog.alert({
+      title: '启动',
+      content: '项目启动中......',
+    });
+  };
+  const onStop = (id) => {
+    Dialog.alert({
+      title: '停止',
+      content: '正在停止......',
+    });
+  };
+
   return (
-    <div id="accordioncourse" className="courselist">
+    <div className="courselist">
       {data.map(project => (
         <div key={project.id} className="card">
-          <div className="card-header" id="headingcourse1">
+          <div className="card-header">
             <h5 className="mb-0">
-              <button className="btn " data-toggle="collapse" data-target="#collapsecourse1" aria-expanded="true" aria-controls="collapsecourse1">
+              <button className="btn" data-toggle="collapse" data-target="#courses" aria-expanded="true" aria-controls="courses">
                 {project.title}
                 <span style={{ fontSize: 13, marginLeft: 10 }}>(耗时：{project.timeConsume})</span>
               </button>
-              <a href="#" className="palyico" data-toggle="modal" data-target="#studentlist">▶</a>
-              <a href="#" className="stopico">▪</a>
+              {/* eslint-disable */}
+              <a href="javascript:void(0);" disabled onClick={() => onStart(project.id)} className="palyico">▶</a>
+              <a href="javascript:void(0);" onClick={() => onStop(project.id)} className="stopico">▪</a>
+              {/* eslint-enable */}
             </h5>
           </div>
-          <div id="collapsecourse1" className="collapse " aria-labelledby="headingcourse1" data-parent="#accordioncourse">
+          <div id="courses" className="collapse">
             {get(project, 'labs', []).map(lab => (
               <div key={lab.id} className="list-group">
-                <a className="list-group-item list-group-item-action " data-toggle="modal" data-target="#studentlist"><span className="learnLesson all"><i /></span>→ {lab.name}</a>
+                {project.running ? (
+                  <a href={lab.url} target="_blank" rel="noopener noreferrer" className="list-group-item list-group-item-action">{lab.name}</a>
+                ) : (
+                  <a onClick={onClick} className="list-group-item list-group-item-action">{lab.name}</a>
+                )}
               </div>
             ))}
           </div>
--- src/pages/Classroom/index.js
+++ src/pages/Classroom/index.js
@@ -2,10 +2,10 @@ import React, { Fragment, useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import queryString from 'query-string';
 import moment from 'moment';
+import { Tab } from '@alifd/next';
 import { classroom, invitation } from '@/utils/api';
 import consts from '@/utils/consts';
 import { getCurrentUser } from '@/utils/helper';
-import Tab from '@/components/Tab';
 import ProjectList from '@/components/ProjectList';
 import TeacherList from '@/components/TeacherList';
 
@@ -85,13 +85,13 @@ export default ({ history }) => {
           </div>
         </div>
       </div>
-      <Tab>
-        <Tab.Item title="详情" className="bg-white">
+      <Tab navStyle={{ padding: '0 10%' }} contentStyle={{ padding: '30px 0' }}>
+        <Tab.Item title="详情">
           <div className="container text-left m-t-60 p-b-60">
             <div className="row">
               <div className="col-12 col-md-8 m-b-30">
                 <h2 className="m-b-20" id="t-project">实验项目</h2>
-                <ProjectList data={course.projects} />
+                <ProjectList course={course} data={course.projects} />
                 <div className="card">
                   <div className="card-header" id="headingcourse4">
                     <h5 className="mb-0">
@@ -149,7 +149,7 @@ export default ({ history }) => {
           </div>
         </Tab.Item>
         {user && user.role === consts.user.TEACHER ? (
-          <Tab.Item title="学生" className="bg-white">
+          <Tab.Item title="学生">
             <div className="container text-left m-t-60 p-b-60">
               <div className="row p-b-60">
                 <div className="col-sm-12">
