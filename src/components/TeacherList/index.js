424de9dafb8240bce9eaf4437f57feb5fc390964
pixcai <pixcai@163.com>
2019-04-17 16:15:39 +0800

    Update assistant info

--- src/components/TeacherList/index.js
+++ src/components/TeacherList/index.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 export default ({ data }) => (
   <div className="row m-t-40 ">
-    {(data || []).map(({ id, thumb, name, remark }) => (
+    {(data || []).map(({ id, thumb, realname, remark, profession }) => (
       <div key={id} className="media col">
         <div className="media-left p-r-40">
-          <Link to="/teacherintroduction"><img className="media-object" alt={name} src={thumb} style={{ width: 128, height: 128 }} /></Link>
+          <Link to="/teacherintroduction"><img className="media-object" alt={realname} src={thumb} style={{ width: 128, height: 128 }} /></Link>
         </div>
         <div className="media-body ">
-          <h3><Link to="/teacherintroduction">{name}</Link></h3>
-          <p>讲师</p>
+          <h3><Link to="/teacherintroduction">{realname}</Link></h3>
+          <p>{profession}</p>
           <div className="text-secondary">
             <p>{remark}</p>
           </div>
