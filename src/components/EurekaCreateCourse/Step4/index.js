9783d7cb48534c9b2a6cc4b01bb344522b62f214
pixcai <pixcai@163.com>
2019-04-24 14:51:46 +0800

    Update card

--- src/components/EurekaCreateCourse/Step4/index.js
+++ src/components/EurekaCreateCourse/Step4/index.js
@@ -1,92 +1,48 @@
-import React, { Fragment, useState, useEffect } from 'react';
-import { Dialog, Button, Input, Grid, Upload, Table, Message, Pagination } from '@alifd/next';
-import { STUDENT_UPLOAD_URL, invitation } from '@/utils/api';
-import get from 'lodash-es/get';
+import React from 'react';
+import { Grid, Button, Dialog } from '@alifd/next';
+import { classroom as classroomAPI } from '@/utils/api';
 
 const { Row, Col } = Grid;
 
-export default ({ getClassroom, toNext, labelSpan, wrapperSpan }) => {
-  const [visible, setVisible] = useState(false);
-  const [students, setStudents] = useState([]);
-  const [pagination, setPagination] = useState({ total: 0, current: 1, pageSize: 10 });
-  const [isLoading, setIsLoading] = useState(false);
-  const [certifications, setCertifications] = useState('');
-  const onSuccess = ({ response }) => setCertifications(`${certifications}\n${response.join('\n')}`);
-  const getStudents = () => {
-    setIsLoading(true);
-    invitation.selectAll({
-      params: {
-        classroom: get(getClassroom(), 'id', ''),
-        embed: 1,
-        page: pagination.current,
-        pageSize: pagination.pageSize,
-      },
-    }).then(({ data }) => {
-      setStudents(data.data);
-      setPagination({ ...pagination, total: data.total });
-      setIsLoading(false);
-    }).catch(() => setIsLoading(false));
-  };
+export default ({ getClassroom, history }) => {
   const onOk = () => {
     const classroom = getClassroom();
 
-    invitation.createBatch({
-      data: {
-        classroom: classroom.id,
-        certifications: certifications.split(/\s/).map(c => c.trim()).filter(c => c),
-      },
-    }).then(() => {
-      setCertifications('');
-      getStudents();
+    Dialog.confirm({
+      title: '创建专题',
+      content: '确定保存专题？',
+      onOk: () => new Promise((resolve) => {
+        setTimeout(resolve, 1500);
+      }).then(() => history.push(`/classroom?id=${classroom.id}`)),
     });
-    setVisible(false);
   };
-  const onDelete = (student) => {
-    invitation.delete({}, { invitationId: student.id }).then(() => {
-      Message.success('删除成功');
-      getStudents();
-    }).catch(() => Message.success('删除失败'));
-  };
-  const onPagination = current => setPagination({ ...pagination, current });
+  const onPublish = () => {
+    const classroom = getClassroom();
 
-  useEffect(() => {
-    getStudents();
-  }, [pagination.current]);
+    Dialog.confirm({
+      title: '创建专题',
+      content: '确定发布专题？',
+      onOk: () => classroomAPI.update({ data: { status: 1 } }, { classroomId: classroom.id })
+        .then(() => history.push(`/classroom?id=${classroom.id}`)),
+    });
+  };
 
   return (
-    <div className="centminheight">
-      <Fragment>
-        <Row justify="center">
-          <Col span={labelSpan + wrapperSpan}>
-            <Button type="primary" onClick={() => setVisible(true)}>导入学生列表</Button>
-            {students ? null : <div className="m-t-20">你还没有添加学生，请添加学生</div>}
-          </Col>
-        </Row>
-        <Row justify="center" className="m-t-20">
-          <Col span={labelSpan + wrapperSpan}>
-            <Table loading={isLoading} dataSource={students}>
-              <Table.Column dataIndex="invitee.certification" title="学生身份信息" />
-              <Table.Column dataIndex="invitee.realname" title="姓名" />
-              <Table.Column width={120} cell={(_, i, student) => <Button type="normal" warning onClick={() => onDelete(student)}>删除</Button>} title="操作" />
-            </Table>
-            <div className="m-t-10" style={{ textAlign: 'center' }}>
-              <Pagination type="simple" {...pagination} onChange={onPagination} />
-            </div>
-          </Col>
-        </Row>
-        <Row justify="center" className="m-t-30">
-          <Col span={4}>
-            <Button type="primary" onClick={toNext} className="serverbtn">下一步</Button>
-          </Col>
-        </Row>
-        <Dialog title="添加学生" shouldUpdatePosition closeable={false} hasMask={false} visible={visible} onOk={onOk} onCancel={() => setVisible(false)} style={{ width: 680 }}>
-          <Upload action={STUDENT_UPLOAD_URL} limit={1} listType="text" onSuccess={onSuccess}>
-            <Button type="primary">上传 .csv 或 .xls(x) 文件</Button>
-          </Upload>
-          <p className="m-t-20">或粘贴学生信息</p>
-          <Input.TextArea value={certifications} onChange={e => setCertifications(e)} style={{ width: '100%' }} rows={16} />
-        </Dialog>
-      </Fragment>
-   </div>
+    <div className="centminheight ">
+      <Row justify="center">
+        <Col span={10} className="text-center">
+          <h5>专题发布</h5>
+          <p className="m-t-20 text-secondary">专题名称：数学文化十讲</p>
+          <div className="alert alert-warning" role="alert">
+            请仔细阅读 <a href="#" className="alert-link">专题</a>. 发布你专题.</div>
+        </Col>
+      </Row>
+      <Row justify="center">
+        <Col span={10} className="m-t-20 text-center">
+          <Button type="secondary" onClick={onOk} className="noserverbtn m-r-10">完成创建保存</Button>
+          <Button type="primary" onClick={onPublish} className="serverbtn ">完成创建并发布</Button>
+        </Col>
+      </Row>
+    </div>
   );
 };
