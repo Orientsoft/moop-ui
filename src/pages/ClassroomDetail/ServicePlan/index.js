942b9c8800a2d5736787510bd5cce351ed17be19
pixcai <pixcai@163.com>
2019-05-14 10:42:23 +0800

    Escape html

--- src/pages/ClassroomDetail/ServicePlan/index.js
+++ src/pages/ClassroomDetail/ServicePlan/index.js
@@ -4,7 +4,7 @@ import Markdown from 'react-markdown';
 export default ({ data = {} }) => {
   return (
     <div className="m-b-30 tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
-      <Markdown source={data.service} />
+      <Markdown source={data.service} escapeHtml={false} />
     </div>
   );
 };
