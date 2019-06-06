9b996b56b713a73abf8c2d0610a03ee9cef5a082
pixcai <pixcai@163.com>
2019-06-06 16:06:57 +0800

    Update tenant tag

--- src/tenant/pages/Tag/components/Table.js
+++ src/tenant/pages/Tag/components/Table.js
@@ -5,10 +5,9 @@ import get from 'lodash-es/get';
 const { Column } = Table;
 
 export default ({ dataSource, onDelete, onQuery, onSelect, ...tableProps }) => {
-  console.log(dataSource)
   return (
     <div style={{ marginTop: 15 }}>
-      <Table dataSource={dataSource} hasBorder={false} {...tableProps}>
+      <Table dataSource={get(dataSource, 'data', [])} hasBorder={false} {...tableProps}>
         <Column title="分类名称" dataIndex="name" />
       </Table>
       <Pagination style={{ marginTop: 15, textAlign: 'center' }} total={get(dataSource, 'meta.total', 0)} pageSize={get(dataSource, 'meta.pageSize', 10)} onChange={page => onQuery({ params: { page } })} />
