898f571d65957fc9b6a4b4c908ec09f67765b203
pixcai <pixcai@163.com>
2019-12-04 15:24:42 +0800

    Fix material url

--- src/components/EurekaCreateCourse/Step1/index.js
+++ src/components/EurekaCreateCourse/Step1/index.js
@@ -65,7 +65,12 @@ export default class Step1 extends React.Component {
         values.description = values.description.toHTML();
         values.requirement = values.requirement.toHTML();
         values.testPoint = values.testPoint.toHTML();
-        values.material = material.filter(m => m.name);
+        values.material = material.filter(m => m.name).map((m) => {
+          if (!/^([a-zA-Z]+):\/\//.test(m.href)) {
+            m.href = `http://${m.href}`;
+          }
+          return m;
+        });
         values.characteristic = characteristic.filter(c => c);
         setData(values);
         toNext();
