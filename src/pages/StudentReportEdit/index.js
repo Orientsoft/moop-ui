74afb5b946c680336964ecafb0abe19ef09be826
黄得高 <huangdegao@huangdegaodeMBP.lan>
2020-11-03 16:40:43 +0800

    提交报告增加默认模版

--- src/pages/StudentReportEdit/index.js
+++ src/pages/StudentReportEdit/index.js
@@ -6,8 +6,10 @@ import { report as reportAPI } from '@/utils/api';
 import { getCurrentUser } from '@/utils/helper';
 import get from 'lodash-es/get';
 
+const defaultstr = '<p style="text-align:center;" class="MsoNormal" align="center"><span style="line-height:3">北京物资学院信息学院实验报告</span></p><p class="MsoNormal"> <span style="line-height:2">课程: </span><u>_                                        </u>  <span style="line-height:2">实验名称:</span><u>_                                </u> <span style="line-height:2">第</span><u>_   </u> <span style="line-height:2">页 ／共</span><u>_   </u> <span style="line-height:2">页 </span></p><p class="MsoNormal"> <span style="line-height:2">系</span> <span style="line-height:2">别:</span><u>_                                                                     </u> <span style="line-height:2">     实验日期:  </span><u>_   </u> <span style="line-height:2">年 </span><u>_   </u><span style="line-height:2">月</span><u>_   </u><span style="line-height:2">日</span></p><p size="1" _root="[object Object]" __ownerid="undefined" __hash="undefined" __altered="false"><span style="line-height:1.5"> 专业班级:</span><u>_                                                               </u>       <span style="line-height:1.5">实验报告日期: </span> <u>_   </u> <span style="line-height:1.5">年 </span> <u>_   </u> <span style="line-height:1.5">月</span> <u>_   </u><span style="line-height:1.5">日</span></p><p class="MsoNormal"><span style="line-height:1.5"> 姓</span> <span style="line-height:1.5">名:</span><u>_                             </u> <span style="line-height:1.5">学号:</span><u>_                             </u> </p><p> </p><hr/><p></p><p class="MsoNormal">一、实验目的</p><p class="MsoNormal"></p><p></p><p></p><p></p><p class="MsoNormal">二、实验内容（描述实验任务）</p><p></p><p></p><p></p><p></p><p class="MsoNormal">三、实验环境（描述实验的软件、硬件环境）</p><p></p><p></p><p></p><p></p><p class="MsoNormal">四、实验步骤与结果</p><p>（描述实现实验内容的步骤和命令即在实验中做了什么事情，怎么做的，结果如何。）</p><p></p><p></p><p></p><p></p><p class="MsoNormal">五、总结</p><p class="MsoNormal">（说明实验过程中遇到的问题及解决办法；个人的收获；未解决的问题等）</p><p></p><p></p><p></p><p></p><p></p><p></p><p></p>'
+
 export default ({ history, location }) => {
-  const [feedback, setFeedback] = useState(null);
+  const [feedback, setFeedback] = useState(BraftEditor.createEditorState(defaultstr));
   const user = getCurrentUser();
   const onBack = () => {
     history.goBack();
