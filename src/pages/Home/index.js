8cbfb4b9251f61d97c8d9556e456f46d03584f96
黄得高 <huangdegao@huangdegaodeMBP.lan>
2020-11-11 11:26:58 +0800

    fix

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,7 +14,7 @@ export default ({history}) => {
   useEffect(() => {
     try {
       //滚动元素id，左切换按钮，右切换按钮，切换元素个数id,滚动方式，切换方向，是否自动滚动，滚动距离，滚动时间，滚动间隔，显示个数
-      // LbMove('BoxUl','btnl','btnr','BoxSwitch',true,'left',true,305,1000,5000,6);
+      LbMove('BoxUl','btnl','btnr','BoxSwitch',true,'left',true,305,1000,5000,6);
     } catch (error) {
       
     }
